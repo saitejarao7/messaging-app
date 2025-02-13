3be2000f71b2553a05279908a0880096e7e47e56
saiteja71997 <saitejarao1997@gmail.com>
2025-02-13 19:23:29 +0530

    VPC steup

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -41,3 +41,158 @@ resource "aws_internet_gateway" "messaging_app-igw" {
     Name = "messaging_app-gw"
   }
 }
+
+resource "aws_route_table" "messaging_app-public-rt" {
+  vpc_id = aws_vpc.messaging_app-vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.messaging_app-igw.id
+  }
+
+  tags = {
+    Name = "messaging_app-public-rt"
+  }
+}
+
+resource "aws_route_table_association" "messaging_app-frontend-asc" {
+  subnet_id      = aws_subnet.messaging_app-frontend-sn.id
+  route_table_id = aws_route_table.messaging_app-public-rt.id
+}
+
+resource "aws_route_table_association" "messaging_app-backend-asc" {
+  subnet_id      = aws_subnet.messaging_app-backend-sn.id
+  route_table_id = aws_route_table.messaging_app-public-rt.id
+}
+
+resource "aws_route_table" "messaging_app-private-rt" {
+  vpc_id = aws_vpc.messaging_app-vpc.id
+
+  tags = {
+    Name = "messaging_app-private-rt"
+  }
+}
+
+resource "aws_route_table_association" "messaging_app-DB-asc" {
+  subnet_id      = aws_subnet.messaging_app-DB-sn.id
+  route_table_id = aws_route_table.messaging_app-private-rt.id
+}
+
+resource "aws_network_acl" "messaging_app-NACL" {
+  vpc_id = aws_vpc.messaging_app-vpc.id
+
+  egress {
+    protocol   = "tcp"
+    rule_no    = 200
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 0
+    to_port    = 65535
+  }
+
+  ingress {
+    protocol   = "tcp"
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 0
+    to_port    = 65535
+  }
+
+  tags = {
+    Name = "messaging_app-NACL"
+  }
+}
+
+resource "aws_network_acl_association" "messaging_app-Nacl-BE-asc" {
+  network_acl_id = aws_network_acl.messaging_app-vpc.id
+  subnet_id      = aws_subnet.messaging_app-backend-sn.id
+}
+
+resource "aws_network_acl_association" "messaging_app-Nacl-FE-asc" {
+  network_acl_id = aws_network_acl.messaging_app-vpc.id
+  subnet_id      = aws_subnet.messaging_app-frontend-sn.id
+}
+
+resource "aws_network_acl_association" "messaging_app-Nacl-DB-asc" {
+  network_acl_id = aws_network_acl.messaging_app-vpc.id
+  subnet_id      = aws_subnet.messaging_app-DB-sn.id
+}
+
+resource "aws_security_group" "messaging_app-FE-SG" {
+  name        = "messaging_app-FE-SG"
+  description = "Allow inbound traffic and all outbound traffic"
+  vpc_id      = aws_vpc.messaging_app-vpc.id
+
+  tags = {
+    Name = "messaging_app-FE-SG"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "messaging_app-FE-SG-ssh" {
+  security_group_id = aws_security_group.messaging_app-FE-SG.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 22
+  ip_protocol       = "tcp"
+  to_port           = 22
+}
+
+resource "aws_vpc_security_group_ingress_rule" "messaging_app-FE-SG-http" {
+  security_group_id = aws_security_group.messaging_app-FE-SG.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 80
+  ip_protocol       = "tcp"
+  to_port           = 80
+}
+
+resource "aws_security_group" "messaging_app-BE-SG" {
+  name        = "messaging_app-BE-SG"
+  description = "Allow inbound traffic and all outbound traffic"
+  vpc_id      = aws_vpc.messaging_app-vpc.id
+
+  tags = {
+    Name = "messaging_app-BE-SG"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "messaging_app-BE-SG-ssh" {
+  security_group_id = aws_security_group.messaging_app-BE-SG.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 22
+  ip_protocol       = "tcp"
+  to_port           = 22
+}
+
+resource "aws_vpc_security_group_ingress_rule" "messaging_app-BE-SG-http" {
+  security_group_id = aws_security_group.messaging_app-BE-SG.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 8080
+  ip_protocol       = "tcp"
+  to_port           = 8080
+}
+
+resource "aws_security_group" "messaging_app-DB-SG" {
+  name        = "messaging_app-DB-SG"
+  description = "Allow inbound traffic and all outbound traffic"
+  vpc_id      = aws_vpc.messaging_app-vpc.id
+
+  tags = {
+    Name = "messaging_app-DB-SG"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "messaging_app-DB-SG-ssh" {
+  security_group_id = aws_security_group.messaging_app-DB-SG.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 22
+  ip_protocol       = "tcp"
+  to_port           = 22
+}
+
+resource "aws_vpc_security_group_ingress_rule" "messaging_app-DB-SG-http" {
+  security_group_id = aws_security_group.messaging_app-DB-SG.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 5432
+  ip_protocol       = "tcp"
+  to_port           = 5432
+}
