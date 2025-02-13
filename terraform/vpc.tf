698563f30f29964b9dd50308c63fd0e9eed9011d
saiteja71997 <saitejarao1997@gmail.com>
2025-02-13 18:02:32 +0530

    create db

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -18,9 +18,18 @@ resource "aws_subnet" "messaging_app-frontend-sn" {
 
 resource "aws_subnet" "messaging_app-backend-sn" {
   vpc_id     = aws_vpc.messaging_app-vpc.id
-  cidr_block = "10.0.1.0/24"
+  cidr_block = "10.0.2.0/24"
   map_public_ip_on_launch = "true"
   tags = {
     Name = "messaging_app-backend-subnet"
+  }
+}
+
+resource "aws_subnet" "messaging_app-DB-sn" {
+  vpc_id     = aws_vpc.messaging_app-vpc.id
+  cidr_block = "10.0.3.0/24"
+  map_public_ip_on_launch = "false"
+  tags = {
+    Name = "messaging_app-database-subnet"
   }
 }
